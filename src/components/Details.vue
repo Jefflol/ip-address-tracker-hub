7185d4412fc11a8740332e409ebcbda9363ad53b
Jefflol <jeffreychen246@gmail.com>
2021-10-09 18:04:08 -0700

    Add Details components

---
+++ src/components/Details.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="details">
+    <DetailsItem name="IP Address" value="192.212.174.101" />
+    <DetailsItem name="Location" value="Brooklyn, NY 10001" />
+    <DetailsItem name="Timezone" value="UTC-05:00" />
+    <DetailsItem name="ISP" value="SpaceX Starlink" />
+</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+import DetailsItem from './DetailsItem.vue'
+
+export default defineComponent({
+    name: 'Details',
+    components: {
+        DetailsItem
+    },
+})
+</script>
+
+<style scoped>
+.details {
+    display: flex;
+    flex-direction: column;
+    row-gap: 20px;
+    width: 260px;
+    border-radius: 15px;
+    padding: 20px 0px;
+    background-color: white;
+    box-shadow: 1px 1px 5px 2px rgba(0, 0, 0, 0.05);
+}
+
+@media only screen and (min-width: 375px) {
+    .details {
+        min-width: 40%;
+    }
+}
+
+@media only screen and (min-width: 1200px) {
+    .details {
+        flex-direction: row;
+        justify-content: space-between;
+        width: max-content;
+        padding: 30px 0;
+        column-gap: 30px;
+    }
+}
+</style>
---
+++ src/components/DetailsItem.vue
@@ -0,0 +1,68 @@
+<template>
+<div class="details__item">
+    <h3>{{ name }}</h3>
+    <p>{{ value }}</p>
+</div>    
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: 'DetailsItem',
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        value: {
+            type: String,
+            required: true
+        }
+    },
+})
+</script>
+
+<style scoped>
+.details__item {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+}
+
+.details__item > h3 {
+    margin: 0;
+    color: hsl(0, 0%, 59%);
+    font-size: 10px;
+    text-transform: uppercase;
+}
+
+.details__item > p {
+    margin: 0;
+    color: hsl(0, 0%, 17%);
+    font-size: 18px;
+    font-weight: 500;
+}
+
+@media only screen and (min-width: 1200px) {
+    .details__item {
+        row-gap: 10px;
+        padding: 0 30px;
+        text-align: left;
+    }
+
+    .details__item + .details__item {
+        border-left: 1px solid hsl(0, 0%, 89%);
+    }
+
+    .details__item > h3 {
+        font-size: 12px;
+        font-weight: 500;
+    }
+
+    .details__item > p {        
+        max-width: 180px;
+        font-size: 28px;
+    }
+}
+</style>
