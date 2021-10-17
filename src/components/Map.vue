fc7aaa7488ee7e76737c453bb436d34be7b42dc8
Jefflol <jeffreychen246@gmail.com>
2021-10-16 17:29:43 -0700

    Allow map to be adjusted dynamically

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -3,25 +3,32 @@
 </template>
 
 <script>
-import { defineComponent, watch } from 'vue'
+import { defineComponent, ref, onMounted, watch } from 'vue'
 import { useGeolocation } from '../store/store.js'
 
 export default defineComponent({
     name: 'Map',
     setup() {
+        const map = ref(null)
         const { geolocation, loading } = useGeolocation()
+
+        const mapTile = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: import.meta.env.VITE_ACCESS_TOKEN
+        })
+
+        onMounted(() => {
+            map.value = L.map('mapid')
+        })
 
         watch(loading, (oldState, newState) => {
             if (!oldState && newState) {
-                let mymap = L.map('mapid').setView(geolocation.value.latlng, 13)
-                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-                        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-                    maxZoom: 18,
-                    id: 'mapbox/streets-v11',
-                    tileSize: 512,
-                    zoomOffset: -1,
-                    accessToken: import.meta.env.VITE_ACCESS_TOKEN
-                }).addTo(mymap)
+                map.value.setView(geolocation.value.latlng, 13)
+                mapTile.addTo(map.value)
             }
         })
     },
